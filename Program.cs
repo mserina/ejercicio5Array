384f4ee88dc2950a06d97659678f1ad6bdb762c5
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2024-01-08 14:35:55 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercicio5Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// DECLARAR 3 VECTORES DE 5 ENTEROS CADA UNO
+            int[] vector1 = new int[5];
+            int[] vector2 = new int[5];
+            int[] vector3 = new int[5];
+
+            /// Pedir al usuario los valores del vector 1 y 2
+            
+             foreach (int i in vector1)
+             {
+                    Console.WriteLine("Escribe numeros para el vector 1");
+                    vector1[i] = Convert.ToInt32(Console.ReadLine());
+             }
+            Console.WriteLine(vector1[1]);
+        }
+    }
+}
